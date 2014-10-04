a44f11cc35b077ee30f24456e1173f08cc2bb9c3
Harry Moreno <morenoh149@gmail.com>
2014-10-04 04:25:43 -0700

    added support for outgoing messages. support for speaking to specified channels

--- config-example.js
+++ config-example.js
@@ -1,19 +1,42 @@
 var slackbot = require('./lib/bot');
+var http = require('http');
+var querystring = require('querystring');
 
 var config = {
     server: 'irc.freenode.com',
     nick: 'slackbot',
     username: 'slackbot-username',
-    token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX',
+    token: process.env.SLACK_INCOMING_TOKEN,
     channels: {
-        '#irc-channel password(optional)': '#slack-channel'
+        '#noisebridge': '#irc'
     },
     users: {
-        '~irclogin': 'slackuser'
-    }
+    },
     // optionals
-    // silent: false // keep the bot quiet
+    floodProtection: true,
+    silent: false // keep the bot quiet
 };
 
 var slackbot = new slackbot.Bot(config);
 slackbot.listen();
+
+
+var server = http.createServer(function (req, res) {
+  if (req.method == 'POST') {
+    req.on('data', function(data) {
+      var payload = querystring.parse(data.toString());
+      if (payload.token == process.env.SLACK_OUTGOING_TOKEN) {
+        console.log('valid post from slack!');
+        var ircMsg = payload.user_name + " says:" + payload.text.slice(4);
+        console.log("attempt to post to irc: ", ircMsg);
+        slackbot.speak('#noisebridge', ircMsg);
+      }
+    });
+  } else {
+    console.log('recieved request (not post)');
+  }
+  res.end('done');
+});
+
+server.listen(5555);
+console.log("Server running at http://localhost:5555/");
